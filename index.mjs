67d03570e0865e697b9a1be695925582e105f787
Prakhar Maurya <000.excel@gmail.com>
2026-04-24 18:15:04 +0530

    feat: add OTP functionality to User model and implement password recovery
    - Updated User model to include optional OTP field.
    - Modified Prisma schema and generated files to reflect changes in User model.
    - Implemented JWT token generation on user login.
    - Added endpoint for password recovery that generates and sends OTP via email.
    - Integrated Resend service for sending OTP emails.
    - Updated package dependencies to include jsonwebtoken and resend.

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import express from 'express'
 import { prisma } from "./prisma/prisma_client.mjs"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { sendOTP } from "./resend.mjs"
 const PORT = 5000
 const app = express()
 app.use(express.json())
@@ -37,8 +40,52 @@ app.post("/login", async (req, res) => {
     })
     return
   }
-  // TODO: generate token
-  res.json({ message: `login successful, welcome ${user.name}` })
+
+  const token = jwt.sign(
+    { id: user.id, name: user.name, email: user.email },
+    process.env.TOKEN_SECRET
+  );
+  res.json({
+    message: `login successful, welcome ${user.name}`,
+    token: token
+  })
+})
+
+app.patch("/forget_password", async (req, res) => {
+  // 1. find user in DB via email
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.body.email
+    },
+  })
+  if (!user) {
+    res.status(404).json({
+      error: "user not found"
+    })
+    return
+  }
+
+  // 2. generate OTP 100-000 , 999-999
+  const opt = Math.floor(Math.random() * 899999 + 100000)
+  const strOTP = `${opt}`
+
+  // // 3.0 Create OTP Column in DB (One time)
+  // // 3.1 save OTP in DB
+  await prisma.user.update({
+    where: {
+      id: user.id
+    },
+    data: {
+      otp: strOTP
+    }
+  })
+
+
+  // 4. send email
+  await sendOTP(user.email, strOTP)
+
+  // // 5. send success in response
+  res.json({ message: "check your email" })
 })
 
 app.listen(PORT, () => {
