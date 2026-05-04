90d9dce6e909e279683d84630203de4aecda2594
Prakhar Maurya <000.excel@gmail.com>
2026-05-04 18:22:41 +0530

    dummy menu routes added

--- index.mjs
+++ index.mjs
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import express from 'express'
 import { userRouter } from "./user/router.mjs";
+import { menuRouter } from "./menu/router.mjs";
 const PORT = 5000
 const app = express()
 
 app.use(express.json())
 
 app.use("/users", userRouter)
+app.use("/menus", menuRouter)
 
 app.listen(PORT, () => {
   console.log(`server started on ${PORT}`)
---
+++ menu/router.mjs
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+const menuRouter = Router()
+import { createMenu, getMenus, updateMenu, deleteMenu } from './controller.mjs'
+
+menuRouter.post("/", createMenu)
+  .get("/", getMenus)
+  .patch("/", updateMenu)
+  .delete("/", deleteMenu)
+
+export { menuRouter }
